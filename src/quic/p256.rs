e3ba7f32f768a20a282fc5c91f6703f71dd60f30
iambhuv <notbhuvnesh@gmail.com>
2025-11-20 15:00:57 +0530

    feat: certgen

--- src/quic/p256.rs
+++ src/quic/p256.rs
@@ -1,11 +1,16 @@
 use std::{fs, path::Path};
 
 use anyhow::Result;
-use p256::SecretKey;
+use p256::{SecretKey, pkcs8::DecodePrivateKey};
 
 
 /// Tries to read a valid SEC1 PEM Private key at `key_path`
 pub fn secret_from_key(key_path: &Path) -> Result<SecretKey> {
     let sec = fs::read_to_string(key_path)?;
-    Ok(SecretKey::from_sec1_pem(&sec)?)
+
+    if sec.starts_with("-----BEGIN EC PRIVATE KEY-----") {
+        Ok(SecretKey::from_sec1_pem(&sec)?)
+    } else {
+        Ok(SecretKey::from_pkcs8_pem(&sec)?)
+    }
 }
