b8e1bfc103339f8e7133ea9920cf02b3add98917
Bhuvnesh <notbhuvnesh@gmail.com>
2026-01-24 23:45:10 +0530

    feat: completed p2p identity proto

--- common/src/proto/client_peer.rs
+++ common/src/proto/client_peer.rs
@@ -1,11 +1,19 @@
-//! Client to Client Proto
+//! Client to Client (P2P) Proto
 
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
+use serde::Serialize;
 use tokio::io::AsyncWriteExt;
 
-use crate::proto::pack::{Packable, Packer};
+use crate::proto::pack::Packable;
+use crate::proto::pack::Packer;
 
 /// Packets for exchanging identity over peer to peer network
+///
+/// Exchange Flow:
+/// 1. [`libcore:..:IdentityQr`] is shared.
+/// 2. Scanner will connect to sharer and send [`IdentityP::AddMe`]
+/// 3. Sharer can either send [`IdentityP::No`] or [`IdentityP::AddedYou`]
+/// 4. Scanner will also save epk of sharer if [`IdentityP::AddedYou`]
 #[derive(Serialize, Deserialize, Debug)]
 pub enum IdentityP {
     AddMe {
@@ -14,6 +22,15 @@ pub enum IdentityP {
         #[serde(with = "serde_bytes")]
         epk: [u8; 32],
         name: String,
+    },
+    /// Rejection for [`IdentityP::AddMe`]
+    No { reason: String },
+
+    /// Proceeding with [`IdentityP::AddMe`]
+    AddedYou {
+        /// Ephemeral key of sharer as it was not included in the IdentityQr
+        #[serde(with = "serde_bytes")]
+        epk: [u8; 32],
     },
 }
 
